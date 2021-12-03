1aebd2204f812af9bfacc2baab267f50d252784b
ravi1728 <ravibhusari.99@gmail.com>
2021-12-03 05:56:50 +0530

    start integraring camerax

--- app/src/main/java/com/edunomics/drawsiness_det/MainActivity.java
+++ app/src/main/java/com/edunomics/drawsiness_det/MainActivity.java
@@ -1,17 +1,26 @@
 package com.edunomics.drawsiness_det;
 
+import static org.opencv.core.Core.ROTATE_90_COUNTERCLOCKWISE;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.util.Pools;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.chaquo.python.PyObject;
@@ -30,6 +39,13 @@ import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,12 +56,22 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     int counter = 0;
     private static final String TAG = "MainActivityTAG";
     private static final int PERMISSION_REQUESTS = 1;
+    Context context;
+    String path;
+
+//    static {
+//       System.loadLibrary("drawsiness_det");
+//    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        context = getApplicationContext();
+        checkFirstRun();
 
         if (!Python.isStarted())
         {
@@ -58,9 +84,11 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
         }
 
+        Button button = findViewById(R.id.button1);
+
 
         cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.CameraView);
-        cameraBridgeViewBase.setCameraIndex(1);
+        cameraBridgeViewBase.setCameraIndex(0);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
         cameraBridgeViewBase.setCvCameraViewListener(this);
 
@@ -81,7 +109,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                         break;
                 }
 
-
             }
 
         };
@@ -94,53 +121,252 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     }
 
+    public final void saveMat(String path, Mat mat) {
+        File file = new File(path).getAbsoluteFile();
+        file.getParentFile().mkdirs();
+        try {
+            int cols = mat.cols();
+            float[] data = new float[(int) mat.total() * mat.channels()];
+            mat.get(0, 0, data);
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path))) {
+                oos.writeObject(cols);
+                oos.writeObject(data);
+                oos.close();
+            }
+        } catch (IOException | ClassCastException ex) {
+            System.err.println("ERROR: Could not save mat to file: " + path);
+            Log.e("function_saveMatTAG", String.valueOf(ex));
+        }
+    }
+
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
         Mat frame = inputFrame.rgba();
+//        frame = frame.;
 
-
-        Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
-
-        byte[] b = new byte[(int) (frame.total() * frame.channels())];
-        frame.get(0, 0,b);
-
-        int r=frame.cols();
-        int c=frame.rows();
-        int ch=frame.channels();
-
-        Log.d(TAG, "bytearraycreated");
-
-
-        Python python = Python.getInstance();
-        PyObject pythonFile = python.getModule("utils");
-
-        PyObject array1 = PyObject.fromJava(b);
-        PyObject result = pythonFile.callAttr("fun1",array1,r,c,ch);
-
-        //frame=(Mat)result;
-
-        Imgproc.putText(frame,
-                result.toString(),
-                new Point(10, 50),               // point
-                Core.FONT_HERSHEY_SIMPLEX ,      // front face
-                1,                               // front scale
-                new Scalar(0, 0, 0),             // Scalar object for color
-                4
-        );
-
-        counter = counter + 1;
-
-
-
-
-
-        Log.d(TAG,"Frame Written");
-
-
+        Core.rotate(frame, frame, ROTATE_90_COUNTERCLOCKWISE);
         return frame;
+
+
+//        Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2BGR);
+//
+//        byte[] b = new byte[(int) (frame.total() * frame.channels())];
+//        frame.get(0, 0,b);
+//
+//        int r=frame.cols();
+//        int c=frame.rows();
+//        int ch=frame.channels();
+//        Log.d(TAG, "InitSize: " + String.valueOf(frame.total() * frame.channels()));
+////        resources.openRawResource(R.raw.filename)
+//
+////        Log.d(TAG, "bytearraycreated");
+////        prototxt = loadUrl("file:///android_res/MobileNetSSD_deploy.prototxt.txt");
+//
+//
+//        Python python = Python.getInstance();
+//        PyObject pythonFile = python.getModule("utils");
+//
+//        PyObject byteFrame = PyObject.fromJava(b);
+//        PyObject result = pythonFile.callAttr("fun1",byteFrame, r ,c, ch);
+////        PyObject.
+//
+//        Log.d(TAG, result.toString());
+//        byte[] b1 = new byte[(int) (frame.total() * frame.channels())];
+//        b1 = result.toJava(b.getClass());
+//        frame.put(0, 0, b1);
+//        Log.d(TAG, "FinalSize: " + String.valueOf(frame.total() * frame.channels()));
+//
+//
+//        // try-catch block to handle exceptions
+////        try {
+////
+////            // Create a file object
+////            File f = new File(String.valueOf(result));
+////
+////            // Get all the names of the files present
+////            // in the given directory
+////            File[] files = f.listFiles();
+////
+//////            System.out.println("Files are:");
+////
+////            // Display the names of the files
+////            for (int i = 0; i < files.length; i++) {
+////                Log.d("File_" + TAG, files[i].getName());
+////            }
+////        }
+////        catch (Exception e) {
+////            System.err.println(e.getMessage());
+////        }
+//
+//
+////        try {
+////            Files.list(new File(String.valueOf(result)).toPath())
+////                    .limit(10)
+////                    .forEach(path -> {
+////                        Log.d(TAG, String.valueOf(path));
+////                    });
+////        } catch (IOException e) {
+////            e.printStackTrace();
+////            Log.d(TAG, String.valueOf(e));
+////        }
+////
+////        //frame=(Mat)result;
+////        Log.d(TAG, frame.size().toString());
+////        Imgproc.putText(frame,
+//////                result.toString(),
+////                "CENTER",
+////                new Point((int)1024/2, (int)768/2),               // point
+////                Core.FONT_HERSHEY_SIMPLEX ,      // front face
+////                10,                               // front scale
+////                new Scalar(123, 210, 95),             // Scalar object for color
+////                4
+////        );
+////
+////        counter = counter + 1;
+////        Log.d(TAG,"Frame Written");
+//        return frame;
     }
 
+    private void checkFirstRun() {
+
+        final String PREFS_NAME = "MyPrefsFile";
+        final String PREF_VERSION_CODE_KEY = "version_code";
+        final int DOESNT_EXIST = -1;
+
+        // Get current version code
+        int currentVersionCode = BuildConfig.VERSION_CODE;
+
+        // Get saved version code
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        int savedVersionCode = prefs.getInt(PREF_VERSION_CODE_KEY, DOESNT_EXIST);
+
+        // Check for first run or upgrade
+        if (currentVersionCode == savedVersionCode) {
+            Log.d("function_checkFirstRunTAG", "NORMAL RUN");
+            // This is just a normal run
+//            try {
+//                Log.d("function_checkFirstRunTAG", "new install");
+//                _copydatabase();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                Log.e("function_copydatabaseTAG", "ERROR _copydatabase" + String.valueOf(e));
+//            }
+            copyAssets();
+            return;
+
+        } else if (savedVersionCode == DOESNT_EXIST) {
+
+            // TODO This is a new install (or the user cleared the shared preferences)
+//            try {
+//                Log.d("function_checkFirstRunTAG", "new install");
+////                _copydatabase();
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                Log.e("function_copydatabaseTAG", "ERROR _copydatabase");
+//            }
+            copyAssets();
+
+        }
+//        else if (currentVersionCode > savedVersionCode) {
+//
+//            // TODO This is an upgrade
+//        }
+
+        // Update the shared preferences with the current version code
+        prefs.edit().putInt(PREF_VERSION_CODE_KEY, currentVersionCode).apply();
+    }
+
+//    Context context = getApplicationContext();
+    public final String modelName = "mobilenetssd_deploy.caffemodel";
+
+    private void copyAssets() {
+        AssetManager assetManager = getAssets();
+        String[] files = null;
+        try {
+            files = assetManager.list("");
+        } catch (IOException e) {
+            Log.e("tag", "Failed to get asset file list.", e);
+        }
+        for(String filename : files) {
+            InputStream in = null;
+            OutputStream out = null;
+            Log.d("FileNameTAG", filename);
+            try {
+                in = assetManager.open(filename);
+                File outFile = new File(context.getDataDir().getAbsolutePath(), filename);
+                out = new FileOutputStream(outFile);
+                copyFile(in, out);
+                in.close();
+                in = null;
+                out.flush();
+                out.close();
+                out = null;
+            } catch(IOException e) {
+//                Log.e("tag", "Failed to copy asset file: " + filename, e);
+            }
+        }
+    }
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+
+//    public void _copydatabase() throws IOException {
+////        context.getFilesDir()
+////        context.getDataDir().getAbsolutePath()
+//        path = context.getDataDir() + "/models/" ; //"/data/data/com.aliserver.shop/databases/";
+//        Log.d("function_copydatabaseTAG", path);
+//
+//        File file = new File(path);
+//        if(!file.exists())
+//            file.createNewFile();
+//            Log.d("function_copydatabaseTAG", "created new");
+//
+//
+//
+//        OutputStream myOutput = new FileOutputStream(path);
+//        byte[] buffer = new byte[1024];
+//        int length;
+////        InputStream myInput = context.getAssets().open("mobilenetssd_deploy.caffemodel");
+////        while ((length = myInput.read(buffer)) > 0) {
+////            myOutput.write(buffer, 0, length);
+////        }
+////        myInput.close();
+////        myOutput.flush();
+////        myOutput.close();
+//
+//        InputStream myInput = context.getAssets().open("mobilenetssd_deploy.prototxt.txt");
+//        while ((length = myInput.read(buffer)) > 0) {
+//            myOutput.write(buffer, 0, length);
+//        }
+//        myInput.close();
+//        myOutput.flush();
+//        myOutput.close();
+//
+//    }
+
+//    View.onCli\\\
+//    @Override
+    public void buttonClick(View view)
+    {
+        Uri selectedUri = Uri.parse(path);
+        Intent intent = new Intent(Intent.ACTION_VIEW, selectedUri);
+//        intent.setDataAndType(selectedUri, "resource/folder");
+
+        if (intent.resolveActivityInfo(getPackageManager(), 0) != null)
+        {
+            startActivity(intent);
+        }
+        else
+        {
+            Log.d("function_buttonClickTAG", "not opened filemanager");
+        }
+    }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
@@ -158,6 +384,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     protected void onResume() {
         super.onResume();
 
+
+
         if (!OpenCVLoader.initDebug()){
             Toast.makeText(getApplicationContext(),"There's a problem, yo!", Toast.LENGTH_SHORT).show();
         }
@@ -166,8 +394,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         {
             baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
         }
-
-
 
     }
 
--- app/src/main/python/utils.py
+++ app/src/main/python/utils.py
@@ -1,18 +1,300 @@
 
 import cv2
 import numpy as np
-import imutil
+import imutils
+import numpy as np
+from imutils import contours, perspective
+from imutils.video import FPS, VideoStream
+from scipy.spatial import distance as dist
+from com.chaquo.python import Python
+import os
+from scipy import io
+from io import BytesIO
 
-def fun1(frame_bgr,r,c,ch):
 
-    frame_bgr1=np.array(frame_bgr,dtype=np.uint8)
-    frame_bgr1=np.reshape(frame_bgr1, (r,c,ch))
+def fun1(byteFrame, r, c, ch):
+    context = Python.getPlatform().getApplication()
+    # frame=np.array(frame)
+    # f = cv2.CreateMat(frame)
+    # frame = np.asarray( frame[:,:] )
+    # return type(byteFrame)
+    # np_bytes = BytesIO(byteFrame)
+    # frame = np.load(np_bytes, allow_pickle=True)
+    frame = np.array(byteFrame, dtype=np.uint8)
+    frame.resize(c, r, ch)
+    orig = frame.copy()
+    # return frame.shape[0]*frame.shape[1]*frame.shape[2]
+    # frame = io.loadmat(frame)
+                   # ,dtype=np.uint8)
+    # np_bytes = frame.tobytes()
+
+    # np_bytes = BytesIO()
+    # np.save(np_bytes, frame, allow_pickle=True)
+    # np_bytes = frame.tobytes()
+    # return np_bytes
+    # return np_bytes.getvalue()
+
+    # return frame[0:100, 0:100, 0]
+    # frame_bgr1=np.reshape(frame_bgr1, (r,c,ch))
 
 
-    frame_gray=None
-    frame_gray=cv2.cvtColor(frame_bgr1, cv2.COLOR_BGR2GRAY)
+    # frame_gray=None
+    # frame_gray=cv2.cvtColor(frame_bgr1, cv2.COLOR_BGR2GRAY)
 
-    if frame_gray is not None:
-        return "SUCCESS"
-    else:
-        return "FAIL"
+    # if frame_gray is not None:
+    #     return "SUCCESS"
+    # else:
+    #     return "FAIL"
+
+    basepath = str(context.getDataDir().getAbsolutePath())
+               # + '/models/'
+
+    # return basepath
+
+
+    prototxt = basepath + "/mobilenetssd_deploy.prototxt.txt"
+    model = basepath + "/mobilenetssd_deploy.caffemodel"
+
+    net = cv2.dnn.readNet(
+        # args["prototxt"],
+        prototxt,
+        # args["model"],
+        model
+    )
+
+    frame = imutils.resize(frame, width=1000)
+
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5
+    )
+
+    # pass the blob through the network and obtain the detections and
+    # predictions
+
+    net.setInput(blob)
+    detections = net.forward()
+    for i in np.arange(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.2:
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+
+    ################## Size Prediction #################################
+    # load the image, convert it to grayscale, and blur it slightly
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    # perform edge detection, then perform a dilation + erosion to
+    # close gaps in between object edges
+    edged = cv2.Canny(gray, 50, 100)
+    edged = cv2.dilate(edged, None, iterations=1)
+    edged = cv2.erode(edged, None, iterations=1)
+    # find contours in the edge map
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0]
+    # sort the contours from left-to-right and initialize the
+    # 'pixels per metric' calibration variable
+    prev_cnts = None
+    try:
+        (cnts, _) = contours.sort_contours(cnts)
+        prev_cnts = cnts
+    except ValueError:
+        cnts = prev_cnts
+    # (cnts, _) = contours.sort_contours(cnts)
+    pixelsPerMetric = None
+
+    # loop over the contours individually
+    # cv2.circle(orig, (0, 0), 500, (0, 0, 255), -1)
+    # cv2.rectangle(orig, (0, 0), (1000, 1000), (800, 132, 109), -1)
+    if cnts is not None:
+        for c in cnts:
+            # if the contour is not sufficiently large, ignore it
+            if cv2.contourArea(c) < 100:
+                continue
+            # compute the rotated bounding box of the contour
+            # orig = frame.copy()
+            box = cv2.minAreaRect(c)
+            box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+            box = np.array(box, dtype="int")
+
+            # ordering the points in the contour such that they appear
+            # in top-left, top-right, bottom-right, and bottom-left
+            # order, then draw the outline of the rotated bounding
+            # box
+            box = perspective.order_points(box)
+            cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+
+            # loop over the original points and draw them
+            for (x, y) in box:
+                cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+
+
+
+            # unpacking the ordered bounding box, then compute the midpoint
+            # between the top-left and top-right coordinates, followed by
+            # the midpoint between bottom-left and bottom-right coordinates
+
+            (tl, tr, br, bl) = box
+            (tltrX, tltrY) = midpoint(tl, tr)
+            (blbrX, blbrY) = midpoint(bl, br)
+
+            # calculating the midpoint between the top left and right upper points,
+            # then midpoint right upper and lower right point
+            (tlblX, tlblY) = midpoint(tl, bl)
+            (trbrX, trbrY) = midpoint(tr, br)
+
+            # draw the midpoints on the image
+            cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+            cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+
+            # draw lines between the midpoints
+            cv2.line(
+                orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)), (255, 0, 255), 2
+            )
+            cv2.line(
+                orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)), (255, 0, 255), 2
+            )
+
+            # compute the Euclidean distance between the midpoints
+            dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+            dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+
+            # if args["unit"] == "cm":
+                # converting to cm
+            if True:
+                dimA = dA * 0.026458
+                dimB = dB * 0.026458
+                dimC = dimA * dimB
+
+                # draw the object sizes on the image
+                cv2.putText(
+                    orig,
+                    "{:.1f}cm".format(dimA),
+                    (int(tltrX - 15), int(tltrY - 10)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65,
+                    (0, 0, 0),
+                    2,
+                )
+                cv2.putText(
+                    orig,
+                    "{:.1f}cm".format(dimB),
+                    (int(trbrX + 10), int(trbrY)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65,
+                    (0, 0, 0),
+                    2,
+                )
+
+                # output text
+                # font = cv2.FONT_HERSHEY_SIMPLEX
+                # cv2.rectangle(orig, (1000, 1000), (700, 620), (800, 132, 109), -1)
+                # cv2.putText(
+                #     orig,
+                #     "Volume: " + "{:.2f} m^2".format(dimC),
+                #     (700, 650),
+                #     font,
+                #     0.7,
+                #     (0xFF, 0xFF, 0x00),
+                #     1,
+                #     cv2.FONT_HERSHEY_SIMPLEX,
+                #     )
+
+                # cv2.putText(
+                #     orig,
+                #     "Orig Length: ",
+                #     # + format(args["length"]),
+                #     (700, 690),
+                #     font,
+                #     0.7,
+                #     (0xFF, 0xFF, 0x00),
+                #     1,
+                #     cv2.FONT_HERSHEY_SIMPLEX,
+                #     )
+                # cv2.putText(
+                #     orig,
+                #     "Orig Width: " ,
+                #     # + format(args["width"]),
+                #     (700, 730),
+                #     font,
+                #     0.7,
+                #     (0xFF, 0xFF, 0x00),
+                #     1,
+                #     cv2.FONT_HERSHEY_SIMPLEX,
+                #     )
+
+
+            # elif args["unit"] == "m":
+            #     # converting to meters
+            #     dimA = dA * 0.000264583
+            #     dimB = dB * 0.000264583
+            #     dimC = dimA * dimB
+            #     cv2.putText(
+            #         orig,
+            #         "{:.1f}m".format(dimA),
+            #         (int(tltrX - 15), int(tltrY - 10)),
+            #         cv2.FONT_HERSHEY_SIMPLEX,
+            #         0.65,
+            #         (255, 255, 255),
+            #         2,
+            #     )
+            #     cv2.putText(
+            #         orig,
+            #         "{:.1f}m".format(dimB),
+            #         (int(trbrX + 10), int(trbrY)),
+            #         cv2.FONT_HERSHEY_SIMPLEX,
+            #         0.65,
+            #         (255, 255, 255),
+            #         2,
+            #     )
+            #     # output text
+            #     font = cv2.FONT_HERSHEY_SIMPLEX
+            #
+            #     cv2.rectangle(orig, (1500, 1600), (700, 620), (800, 132, 109), -1)
+            #     cv2.putText(
+            #         orig,
+            #         "Volume: " + "{:.2f} m^2".format(dimC),
+            #         (700, 650),
+            #         font,
+            #         0.7,
+            #         (0xFF, 0xFF, 0x00),
+            #         1,
+            #         cv2.FONT_HERSHEY_SIMPLEX,
+            #         )
+            #
+            #     cv2.putText(
+            #         orig,
+            #         "Orig Length: " + format(args["length"]),
+            #         (700, 690),
+            #         font,
+            #         0.7,
+            #         (0xFF, 0xFF, 0x00),
+            #         1,
+            #         cv2.FONT_HERSHEY_SIMPLEX,
+            #         )
+            #     cv2.putText(
+            #         orig,
+            #         "Orig Width: " + format(args["width"]),
+            #         (700, 730),
+            #         font,
+            #         0.7,
+            #         (0xFF, 0xFF, 0x00),
+            #         1,
+            #         cv2.FONT_HERSHEY_SIMPLEX,
+            #         )
+
+    np_bytes = orig.tobytes()
+    return np_bytes
+
+
+# initializing midpoint
+def midpoint(ptA, ptB):
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+
+
